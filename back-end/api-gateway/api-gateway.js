69d0e944ce51aaf542088aa1816df8180d7a8640
Emilly Wolski <emilly.wolski@gmail.com>
2025-06-19 17:45:30 -0300

    refactor: :recycle: Ajuste na api-gateway (post de reserva)

--- back-end/api-gateway/api-gateway.js
+++ back-end/api-gateway/api-gateway.js
@@ -359,11 +359,18 @@ app.get(
 
 app.post(
   '/api/reservas',
-  // requireRole('CLIENTE'),
   createProxyMiddleware({
     target: reservaServiceUrl,
     changeOrigin: true,
     pathRewrite: path => path.replace('/api/reservas', '/ms-reserva/reservas'),
+    onProxyReq: (proxyReq, req) => {
+      if (req.body) {
+        const bodyData = JSON.stringify(req.body);
+        proxyReq.setHeader('Content-Type', 'application/json');
+        proxyReq.setHeader('Content-Length', Buffer.byteLength(bodyData));
+        proxyReq.write(bodyData);
+      }
+    }
   })
 );
 
