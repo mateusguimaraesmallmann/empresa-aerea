86f8b28926cfc138b96be96b0833b32f301d68f7
Mateus Irineu Guimarães Mallmann <mateusmallmann2013@gmail.com>
2025-06-23 17:26:12 -0300

    refactor(backend): revisão da autenticação saga

--- back-end/saga/src/main/java/br/com/empresa_aerea/saga/configurations/RabbitMQConfiguration.java
+++ back-end/saga/src/main/java/br/com/empresa_aerea/saga/configurations/RabbitMQConfiguration.java
@@ -4,7 +4,6 @@ import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
@@ -32,11 +31,6 @@ public class RabbitMQConfiguration {
     public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
         RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
         return rabbitTemplate;
-    }
-
-    @Bean
-    public Jackson2JsonMessageConverter jsonMessageConverter() {
-        return new Jackson2JsonMessageConverter();
     }
 
 }
